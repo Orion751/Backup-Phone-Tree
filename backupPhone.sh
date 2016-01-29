aa2cc48207213ba727632a7c8072f91dab53e655
Orion751 <Orion751@users.noreply.github.com>
2016-01-29 01:52:54 -0500

    Added bacupPhone.sh with pseudocode

---
+++ backupPhone.sh
@@ -0,0 +1,11 @@
+#backup(path)
+#	copy; (See if syntax for bitpim is cp dir dest vs dir dest/newName
+#
+#	if path contains subdirectories
+#		for each subdir
+#			return backup(subdir)
+#	else
+#		return
+#
+#backup("/")
+
